09f3f3834cb9a8c0b39122f19847745a0a24ec00
AmalieKoder <amlie.koder@gmail.com>
2023-11-28 09:32:02 +0100

    index commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// A function when you write a name it should appear in HTML
+
+function submitName() {
+    let nameInput = document.querySelector("#textInput").value;
+    console.log(nameInput);
+
+    let inputP = document.querySelector("#p");
+    inputP.textContent = nameInput;
+}
+
+// When this button hear a click this should be active (submitName)
+
+let button = document.querySelector("#button");
+button.addEventListener("click", submitName);
+
+
+// A function when you write a username, email and password it should appear in HTML when you click this send button
+
+function submitUsernameEmailPassword() {
+    let userName = document.querySelector("#userName").value;
+
+    let inputP = document.querySelector("#p1");
+    inputP.textContent = userName;
+    
+    let email = document.querySelector("#emailInput").value;
+
+    let emailInput = document.querySelector("#p2");
+    emailInput.textContent = email;
+
+    let password = document.querySelector("#passordInput").value;
+
+    let passwordInput = document.querySelector("#p3");
+    passwordInput.textContent = password;
+    
+}
+
+// When this button hear a click this should be active (submitUsernameEmailPassword)
+
+let sendButton = document.querySelector("#sendButton");
+sendButton.addEventListener("click", submitUsernameEmailPassword);
+
